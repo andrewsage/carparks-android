7fb51d5d0fbec08b6f4d64c5d7c2a602c71e1453
Andrew Sage <andrew@asae.co.uk>
2014-07-11 19:07:27 +0100

    Settings added.

--- app/src/main/java/com/xoverto/carparks/app/MainActivity.java
+++ app/src/main/java/com/xoverto/carparks/app/MainActivity.java
@@ -55,6 +55,10 @@ public class MainActivity extends ActionBarActivity implements CarParkFragment.O
             return true;
         }
         if (id == R.id.action_settings) {
+
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
+
             return true;
         }
         return super.onOptionsItemSelected(item);
